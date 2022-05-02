757b44f3f92113c729718fc80a3690f634ecf57a
TDenisaM <99595805+TDenisaM@users.noreply.github.com>
2022-05-02 10:06:12 +0200

    created react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Translate from "./Translate";
+
+import "./css/App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Translate />
+    </div>
+  );
+}
